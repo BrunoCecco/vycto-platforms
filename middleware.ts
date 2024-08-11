2c261662bba721879457706ca0b29cbe97daefc9
Bruno Ceccolini <t-bceccolini@expediagroup.com>
2024-08-11 12:18:33 +0100

    redirect fix

--- middleware.ts
+++ middleware.ts
@@ -46,11 +46,14 @@ export default async function middleware(req: NextRequest) {
       return NextResponse.redirect(new URL("/login", req.url));
     } else if (
       session &&
-      (path == "/login" || path == "/verify") &&
+      (path === "/login" || path === "/verify") &&
       hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
     ) {
-      return NextResponse.redirect(new URL(hostname, req.url));
-    } else if (session) {
+      return NextResponse.redirect(new URL("/", req.url));
+    } else if (
+      session &&
+      hostname != `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
+    ) {
       return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
     }
     return NextResponse.rewrite(
