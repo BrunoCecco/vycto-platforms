9a8dce109ce4a2f63d91d34e5f58ec4bf1d0e9e4
devsalmon <54852257+devsalmon@users.noreply.github.com>
2024-09-04 00:40:44 +0200

    Leaderboard responsiveness

--- components/leaderboard.tsx
+++ components/leaderboard.tsx
@@ -41,15 +41,14 @@ const Leaderboard = ({
 }) => {
   var sortedUsers = users.sort((a: any, b: any) => b.points - a.points);
 
-  if (sortedUsers.length === 0) {
+  if (sortedUsers.length === 1) {
     sortedUsers = leaderboardData;
   }
-  console.log(sortedUsers);
 
   return (
-    <div className="container w-full rounded-2xl border border-gray-200 bg-white p-4 md:p-8">
+    <div className="container w-full border border-gray-200 bg-white px-2 py-6 md:rounded-2xl md:p-8">
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row md:items-center md:py-6">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center justify-center gap-4 md:justify-normal">
           <div className="relative inline-block h-16 w-32 align-middle md:h-16 md:w-36">
             <Image
               src={siteData.logo || "/logo.png"}
@@ -63,27 +62,26 @@ const Leaderboard = ({
             Leaderboard
           </h1>
         </div>
-        {/* <button className="w-24 rounded-full bg-blue-100 p-2 text-purple-800 hover:bg-blue-300">
-          View
-        </button> */}
       </div>
+
       <div className="overflow-x-auto">
-        <table className="min-w-full rounded-xl">
+        {/* Desktop Table */}
+        <table className="hidden min-w-full rounded-xl md:table">
           <thead className="pb-4">
             <tr className="pb-4">
-              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+              <th className="order-2 py-3 text-left text-xs font-medium uppercase text-gray-500">
                 Name
               </th>
-              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+              <th className="order-1 py-3 text-left text-xs font-medium uppercase text-gray-500">
                 Rank
               </th>
-              <th className="text-wrap py-3 text-left text-xs font-medium uppercase text-gray-500">
+              <th className="order-3 text-wrap py-3 text-left text-xs font-medium uppercase text-gray-500">
                 Points
               </th>
-              <th className="hidden py-3 text-left text-xs font-medium uppercase text-gray-500 sm:table-cell">
+              <th className="order-4 hidden py-3 text-left text-xs font-medium uppercase text-gray-500 sm:table-cell">
                 Submission
               </th>
-              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+              <th className="order-5 py-3 text-left text-xs font-medium uppercase text-gray-500">
                 Rank
               </th>
             </tr>
@@ -125,6 +123,59 @@ const Leaderboard = ({
                     className="my-auto flex h-2 items-center justify-center rounded-full bg-purple-800 text-white"
                     style={{ width: 100 - index + 1 + "%" }}
                   ></div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Mobile Table */}
+        <table className="min-w-full rounded-xl md:hidden">
+          <thead className="pb-4">
+            <tr className="pb-4">
+              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500"></th>
+              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+                Name
+              </th>
+              <th className="text-wrap py-3 text-left text-xs font-medium uppercase text-gray-500">
+                Points
+              </th>
+              <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+                Submission
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedUsers.map((user: any, index: number) => (
+              <tr key={user.userId} className="border-b">
+                <td className="pr-2 text-gray-900">{index + 1}</td>
+                <td className="flex items-center py-4">
+                  <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle">
+                    <Image
+                      src={
+                        user.image ?? `https://avatar.vercel.sh/${user.email}`
+                      }
+                      alt="Profile"
+                      fill={true}
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      objectFit="cover"
+                      className="overflow-hidden rounded-full"
+                    />
+                  </div>
+                  <span className="ml-2 font-bold text-gray-900">
+                    @{user.username || user.email || user.name || "User"}
+                  </span>
+                </td>
+                <td className="py-4 text-gray-900">
+                  {parseFloat(user.points || "0").toFixed(2)}
+                </td>
+                <td className="py-4">
+                  <Link
+                    href={`/${competition.slug}/${user.userId}`}
+                    className="rounded-full bg-blue-100 p-2 px-4 text-purple-800 hover:bg-blue-300"
+                  >
+                    View
+                  </Link>
                 </td>
               </tr>
             ))}
