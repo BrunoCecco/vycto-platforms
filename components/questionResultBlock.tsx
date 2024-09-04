e3385becaaf0dded5feb196aa9d131ec0fd927a8
devsalmon <54852257+devsalmon@users.noreply.github.com>
2024-09-05 01:16:22 +0200

    Create questionResultBlock.tsx

---
+++ components/questionResultBlock.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface QuestionResultBlockProps {
+  correctAnswer: any;
+  pointsEarned: any;
+  totalPoints: any;
+}
+
+const QuestionResultBlock: React.FC<QuestionResultBlockProps> = ({
+  correctAnswer,
+  pointsEarned,
+  totalPoints,
+}) => {
+  return (
+    <div className="flex items-center justify-center pt-6">
+      <div className="w-1/2 rounded-lg border border-green-600 p-4 text-center shadow-md">
+        <div className="mb-2">
+          Correct answer:{" "}
+          <span className="font-semibold text-gray-800">{correctAnswer}</span>
+        </div>
+        <div>
+          Points earned:{" "}
+          <span className="font-semibold text-green-600">{pointsEarned}</span>/
+          {totalPoints}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionResultBlock;
