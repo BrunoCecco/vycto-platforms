acd476a467f1912ce14ecef0ceba1e62ff607f47
devsalmon <54852257+devsalmon@users.noreply.github.com>
2024-09-11 00:05:50 +0200

    Add custom bg-colour to site dashboard

--- app/app/(dashboard)/site/[id]/layout.tsx
+++ app/app/(dashboard)/site/[id]/layout.tsx
@@ -1,8 +1,25 @@
 import { ReactNode } from "react";
+import db from "@/lib/db";
 
-export default function SiteLayout({ children }: { children: ReactNode }) {
+export default async function SiteLayout({
+  params,
+  children,
+}: {
+  params: { id: string };
+  children: ReactNode;
+}) {
+  const data = await db.query.sites.findFirst({
+    where: (sites, { eq }) => eq(sites.id, decodeURIComponent(params.id)),
+  });
+
   return (
-    <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
+    <div
+      className="flex max-w-screen-xl flex-col space-y-12 p-8"
+      style={{
+        backgroundColor: data?.color1 ?? "green",
+        minHeight: "100vh",
+      }}
+    >
       <div className="flex flex-col space-y-6">{children}</div>
     </div>
   );
