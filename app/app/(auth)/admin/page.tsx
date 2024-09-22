654c21d99ef4b41ae730f07cdeade90728113806
Bruno Ceccolini <t-bceccolini@expediagroup.com>
2024-09-22 21:57:04 +0100

    login page redirect

---
+++ app/app/(auth)/admin/page.tsx
@@ -0,0 +1,44 @@
+import { Suspense, useState } from "react";
+import { signIn } from "next-auth/react";
+import B2BSignUp from "@/components/auth/b2BSignUp";
+import { getSiteData } from "@/lib/fetchers";
+import { headers } from "next/headers";
+
+export default async function AdminLoginPage({
+  params,
+}: {
+  params: { domain: string };
+}) {
+  const domain = decodeURIComponent(params.domain);
+  const heads = headers();
+  const siteData = await getSiteData(heads.get("x-forwarded-host") || domain);
+
+  return (
+    <B2BSignUp />
+    //   <input
+    //     type="email"
+    //     id="email"
+    //     name="email"
+    //     placeholder="Email address"
+    //     onChange={(e) => setEmail(e.target.value)}
+    //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
+    //   />
+    //   <input
+    //     type="text"
+    //     id="username"
+    //     name="username"
+    //     placeholder="Username"
+    //     onChange={(e) => setUsername(e.target.value)}
+    //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
+    //   />
+
+    //   <Suspense
+    //     fallback={
+    //       <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+    //     }
+    //   >
+    // <LoginButton email={email} username={username} />
+    //   </Suspense>
+    // </div>
+  );
+}
--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import LoginButton from "./loginButton";
 import UserSignUp from "@/components/auth/userSignUp";
 import { getSiteData } from "@/lib/fetchers";
 import { headers } from "next/headers";
+import B2BSignUp from "@/components/auth/b2BSignUp";
 
 export default async function LoginPage({
   params,
@@ -12,34 +13,37 @@ export default async function LoginPage({
 }) {
   const domain = decodeURIComponent(params.domain);
   const heads = headers();
-  const siteData = await getSiteData(heads.get("x-forwarded-host") || domain);
+  const host = heads.get("x-forwarded-host");
+  const siteData = await getSiteData(host || domain);
 
-  return (
+  return host?.includes("app.vycto.com") || host?.includes("app.localhost") ? (
+    <B2BSignUp />
+  ) : (
     <UserSignUp siteData={siteData} />
-    //   <input
-    //     type="email"
-    //     id="email"
-    //     name="email"
-    //     placeholder="Email address"
-    //     onChange={(e) => setEmail(e.target.value)}
-    //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
-    //   />
-    //   <input
-    //     type="text"
-    //     id="username"
-    //     name="username"
-    //     placeholder="Username"
-    //     onChange={(e) => setUsername(e.target.value)}
-    //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
-    //   />
-
-    //   <Suspense
-    //     fallback={
-    //       <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
-    //     }
-    //   >
-    // <LoginButton email={email} username={username} />
-    //   </Suspense>
-    // </div>
   );
+  //   <input
+  //     type="email"
+  //     id="email"
+  //     name="email"
+  //     placeholder="Email address"
+  //     onChange={(e) => setEmail(e.target.value)}
+  //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
+  //   />
+  //   <input
+  //     type="text"
+  //     id="username"
+  //     name="username"
+  //     placeholder="Username"
+  //     onChange={(e) => setUsername(e.target.value)}
+  //     className="mt-1 block w-full rounded-md border border-stone-200 dark:border-stone-400"
+  //   />
+
+  //   <Suspense
+  //     fallback={
+  //       <div className="my-2 h-10 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
+  //     }
+  //   >
+  // <LoginButton email={email} username={username} />
+  //   </Suspense>
+  // </div>
 }
