7910a49bac25ef9154183f8379fe4f47d5e4813e
Bruno Ceccolini <t-bceccolini@expediagroup.com>
2024-09-24 07:54:51 +0100

    banner dimensions

--- components/leaderboard/leaderboard.tsx
+++ components/leaderboard/leaderboard.tsx
@@ -47,7 +47,7 @@ const Leaderboard = ({
   return (
     <div className="container bg-white px-2 py-6 md:rounded-2xl md:p-8">
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row md:items-center md:py-6">
-        <div className="flex items-center justify-center gap-4 md:justify-normal">
+        <div className="flex items-center gap-4">
           <div className="relative inline-block h-16 w-32 align-middle md:h-16 md:w-36">
             <Image
               src={siteData.logo || "/logo.png"}
