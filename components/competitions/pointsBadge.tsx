6e9cb13049d1c9dbfc1b58228cd9674674849efe
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-09-28 19:43:39 +0100

    update

--- components/competitions/pointsBadge.tsx
+++ components/competitions/pointsBadge.tsx
@@ -6,7 +6,7 @@ interface PointsBadgeProps {
 
 const PointsBadge: FC<PointsBadgeProps> = ({ points }) => {
   return (
-    <div className="absolute -top-6 right-0 z-10 flex h-16 w-16 items-center justify-center rounded-full border-4 border-white bg-green-500 text-lg font-bold text-white">
+    <div className="absolute -right-6 -top-6 z-10 flex h-16 w-16 items-center justify-center rounded-full border-4 border-white bg-green-500 text-lg font-bold text-white">
       <div className="flex flex-col items-center">
         <span>{points.toString().padStart(2, "0")}</span>
         <span className="-mt-1 text-xs">Points</span>
