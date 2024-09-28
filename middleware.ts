22201196442ab2fd4747156b33967182c0ecbbcf
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-09-28 19:17:51 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -100,7 +100,7 @@ export default async function middleware(req: NextRequest) {
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
-    return NextResponse.redirect("https://vycto.tech/");
+    return NextResponse.redirect("https://vycto.tech");
     // return NextResponse.rewrite(
     //   new URL(`/home${path === "/" ? "" : path}`, req.url),
     // );
