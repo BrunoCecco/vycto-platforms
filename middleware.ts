0d333280661bc26b951e66e70e9b2ded98f81de1
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-09-29 11:51:54 +0100

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-import { users } from "./lib/schema";
-import db from "./lib/db";
-import { eq } from "drizzle-orm";
-import { updateName, updateUsername } from "./lib/actions";
 
 const SUPER_ADMINS = [
   "bruno.ceccolini@gmail.com",
@@ -13,28 +9,13 @@ const SUPER_ADMINS = [
   "nicolas2ric@gmail.com",
 ];
 
-export const config = {
-  matcher: [
-    /*
-     * Match all paths except for:
-     * 1. /api routes
-     * 2. /_next (Next.js internals)
-     * 3. /_static (inside /public)
-     * 4. all root files inside /public (e.g. /favicon.ico)
-     */
-    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
-  ],
-};
-
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
   let hostname = req.headers
     .get("host")!
     .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
 
-  // special case for Vercel preview deployment URLs
+  // Special case for Vercel preview deployment URLs
   if (
     hostname.includes("---") &&
     hostname.endsWith(`.${process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_SUFFIX}`)
@@ -49,63 +30,64 @@ export default async function middleware(req: NextRequest) {
   const path = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
-  // rewrites for app pages
-  if (
-    hostname.includes(`${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
-    hostname != process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
-    const session = await getToken({ req });
-    if (!session && path.includes("/comp")) {
-      console.log("redirecting to comp" + hostname + path);
-      return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
-    } else if (
-      !session &&
-      path !== "/login" &&
-      path !== "/verify" &&
-      !path.includes("/comp")
-    ) {
-      console.log("redirecting to login");
+
+  const session = await getToken({ req });
+
+  // Handle admin site (app.vyctorewards.com)
+  if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    // Redirect to login if no session and not already on login page
+    if (!session && path != "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
-    } else if (session && (path === "/login" || path === "/verify")) {
-      return NextResponse.redirect(new URL("/", req.url));
-    } else if (
-      session &&
-      hostname != `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
-    ) {
-      return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
     }
-    if (session && SUPER_ADMINS.indexOf(session?.email || "") != -1) {
+
+    // Check if user is a super admin
+    if (session && !SUPER_ADMINS.includes(session.email as string)) {
+      // Redirect to an unauthorized page or the root domain
+      return NextResponse.redirect(
+        process.env.NEXT_PUBLIC_ROOT_DOMAIN as string,
+      );
+    }
+
+    // Allow access for super admins
+    return NextResponse.rewrite(new URL(`/app${path}`, req.url));
+  }
+
+  // Handle user-facing sites ([example].vyctorewards.com)
+  if (hostname.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`)) {
+    console.log("HEREEE", hostname, path, session);
+    if (!session && path.startsWith("/login")) {
       return NextResponse.rewrite(
         new URL(`/app${path === "/" ? "" : path}`, req.url),
       );
-    } else if (session && SUPER_ADMINS.indexOf(session?.email || "") == -1) {
-      return NextResponse.rewrite(
-        new URL(`/home${path === "/" ? "" : path}`, req.url),
-      );
     }
-    return NextResponse.rewrite(
-      new URL(`/app${path === "/" ? "" : path}`, req.url),
-    );
+
+    // Allow all requests to user-facing sites
+    return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
   }
 
-  // special case for `vercel.pub` domain
-  if (hostname === "vercel.pub") {
-    return NextResponse.redirect(
-      "https://vercel.com/blog/platforms-starter-kit",
-    );
-  }
-
-  // rewrite root application to `/home` folder
-  if (
-    hostname === "localhost:3000" ||
-    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
+  // Handle root domain (vyctorewards.com)
+  if (hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
+    // Redirect root domain to vycto.tech
     return NextResponse.redirect("https://vycto.tech");
-    // return NextResponse.rewrite(
-    //   new URL(`/home${path === "/" ? "" : path}`, req.url),
-    // );
   }
 
-  // rewrite everything else to `/[domain]/[slug] dynamic route
   return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+  // For any other hostnames, you can decide to either:
+  // 1. Redirect to the root domain
+  // return NextResponse.redirect(`https://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  // 2. Or show a 404 page
+  // return NextResponse.rewrite(new URL('/404', req.url));
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /_static (inside /public)
+     * 4. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
