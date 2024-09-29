b975200c1d9a9b28dced1a1618bf713eb3433e0a
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-09-29 16:45:56 +0100

    auth update

--- components/auth/userSignUp.tsx
+++ components/auth/userSignUp.tsx
@@ -172,7 +172,13 @@ const UserSignUp = ({
                 className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
               />
             </div> */}
-            <LoginButton email={email} username={username} name={name} />
+            <LoginButton
+              email={email}
+              username={username}
+              localAnswers={localAnswers}
+              competitionSlug={competitionSlug}
+              name={name}
+            />
           </form>
         </div>
 
