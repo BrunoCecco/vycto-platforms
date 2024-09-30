19d7c8e253258b4d7cf0bff17c87f629def27ad5
devsalmon <54852257+devsalmon@users.noreply.github.com>
2024-09-30 12:48:06 +0200

    Improve site leaderboard UI

--- components/leaderboard/mainLeaderboard.tsx
+++ components/leaderboard/mainLeaderboard.tsx
@@ -29,8 +29,8 @@ const MainLeaderboard = ({ siteData }: { siteData: SelectSite }) => {
 
   return (
     data && (
-      <div className="container bg-white p-10 md:rounded-2xl">
-        <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row md:items-center md:py-6">
+      <div className="container min-w-full rounded-xl bg-white px-2 py-4 md:p-10">
+        <div className="flex flex-col justify-between gap-4 sm:flex-row md:items-center md:py-6">
           <div className="flex items-center gap-4">
             <div className="relative inline-block h-16 w-32 align-middle md:h-16 md:w-36">
               <Image
@@ -48,7 +48,7 @@ const MainLeaderboard = ({ siteData }: { siteData: SelectSite }) => {
                 : new Date().getFullYear()}
             </h1>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center justify-center gap-6">
             <button
               onClick={() => setRangeType("monthly")}
               className={`rounded-md px-4 py-2`}
@@ -125,7 +125,9 @@ const MainLeaderboard = ({ siteData }: { siteData: SelectSite }) => {
                 ))
               ) : (
                 <tr>
-                  <td className="py-4 text-gray-900">No data available</td>
+                  <td className="pt-4 text-center text-gray-900">
+                    No data available
+                  </td>
                 </tr>
               )}
             </tbody>
@@ -133,47 +135,53 @@ const MainLeaderboard = ({ siteData }: { siteData: SelectSite }) => {
 
           {/* Mobile Table */}
           <table className="min-w-full rounded-xl md:hidden">
-            <thead className="pb-4">
-              <tr className="pb-4">
-                <th className="py-3 text-left text-xs font-medium uppercase text-gray-500"></th>
-                <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
-                  Name
-                </th>
-                <th className="text-wrap py-3 text-left text-xs font-medium uppercase text-gray-500">
-                  Points
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.length > 0 ? (
-                data.map((user: any, index: number) => (
-                  <tr key={user.userId} className="border-b">
-                    <td className="pr-2 text-gray-900">{index + 1}</td>
-                    <td className="flex items-center py-4">
-                      <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle">
-                        <Image
-                          src={`https://avatar.vercel.sh/${user.username}`}
-                          alt="Profile"
-                          fill={true}
-                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                          className="overflow-hidden rounded-full object-cover"
-                        />
-                      </div>
-                      <span className="ml-2 font-bold text-gray-900">
-                        @{user.username || "User" + index}
-                      </span>
-                    </td>
-                    <td className="py-4 text-gray-900">
-                      {parseFloat(user.points || "0").toFixed(2)}
-                    </td>
+            {data?.length > 0 ? (
+              <>
+                <thead className="pb-4">
+                  <tr className="pb-4">
+                    <th className="py-3 text-left text-xs font-medium uppercase text-gray-500"></th>
+                    <th className="py-3 text-left text-xs font-medium uppercase text-gray-500">
+                      Name
+                    </th>
+                    <th className="text-wrap py-3 text-left text-xs font-medium uppercase text-gray-500">
+                      Points
+                    </th>
                   </tr>
-                ))
-              ) : (
+                </thead>
+                <tbody>
+                  {data.map((user: any, index: number) => (
+                    <tr key={user.userId} className="border-b">
+                      <td className="pr-2 text-gray-900">{index + 1}</td>
+                      <td className="flex items-center py-4">
+                        <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle">
+                          <Image
+                            src={`https://avatar.vercel.sh/${user.username}`}
+                            alt="Profile"
+                            fill={true}
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            className="overflow-hidden rounded-full object-cover"
+                          />
+                        </div>
+                        <span className="ml-2 font-bold text-gray-900">
+                          @{user.username || "User" + index}
+                        </span>
+                      </td>
+                      <td className="py-4 text-gray-900">
+                        {parseFloat(user.points || "0").toFixed(2)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </>
+            ) : (
+              <tbody>
                 <tr>
-                  <td className="py-4 text-gray-900">No data available</td>
+                  <td className="pt-4 text-center text-gray-900" colSpan={3}>
+                    No data available
+                  </td>
                 </tr>
-              )}
-            </tbody>
+              </tbody>
+            )}
           </table>
         </div>
       </div>
