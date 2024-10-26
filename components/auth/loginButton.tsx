d097d139fba572f501c070ee997e2e738ba203d2
Mat <matthewcharke@gmail.com>
2024-10-26 17:14:29 +0100

    google provider

--- components/auth/loginButton.tsx
+++ components/auth/loginButton.tsx
@@ -37,7 +37,7 @@ export default function LoginButton({
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
 
-  const posthog = usePostHog(); // posthog
+  const posthog = usePostHog();
   // Get error message added by next/auth in URL.
   const searchParams = useSearchParams();
 
