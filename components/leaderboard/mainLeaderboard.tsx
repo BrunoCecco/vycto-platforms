d6928fa504e4a4f3c214e673aec11247e1976a45
BrunoCecco <bruno.ceccolini@gmail.com>
2024-11-20 03:14:29 +0000

    updates

--- components/leaderboard/mainLeaderboard.tsx
+++ components/leaderboard/mainLeaderboard.tsx
@@ -22,13 +22,14 @@ const MainLeaderboard = ({
     "last week" | "monthly" | "season" | "all time"
   >("all time");
 
-  const [data, setData] = useState<LeaderboardUser[]>();
-  const [filteredData, setFilteredData] = useState<LeaderboardUser[]>();
+  const [data, setData] = useState<LeaderboardUser[]>([]);
+  const [filteredData, setFilteredData] = useState<LeaderboardUser[]>([]);
   const [query, setQuery] = useState<string>("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const leaderboardData = await getLeaderboardData(siteData.id, rangeType);
       setData(leaderboardData! as LeaderboardUser[]);
       setLoading(false);
@@ -66,19 +67,16 @@ const MainLeaderboard = ({
     }
   };
 
-  return loading ? (
-    <LoadingDots />
-  ) : (
-    filteredData && (
-      <div className="rounded-x container min-w-full text-white">
-        <LeaderboardHeader
-          siteData={siteData}
-          rangeType={rangeType}
-          setRangeType={setRangeType}
-          setQuery={setQuery}
-        />
+  return (
+    <div className="rounded-x container min-w-full text-white">
+      <LeaderboardHeader
+        siteData={siteData}
+        rangeType={rangeType}
+        setRangeType={setRangeType}
+        setQuery={setQuery}
+      />
 
-        {/* {data && data?.findIndex((usr) => usr.id === user?.id) > -1 && (
+      {/* {data && data?.findIndex((usr) => usr.id === user?.id) > -1 && (
           <div className="flex items-center gap-4 rounded-lg py-4">
             <span className="text-lg font-bold">Your Rank:</span>
             <span className="text-lg font-bold">
@@ -87,137 +85,86 @@ const MainLeaderboard = ({
           </div>
         )} */}
 
-        <div className="overflow-x-auto">
-          <table className="min-w-full rounded-xl">
-            <thead className="pb-4">
-              <tr className="pb-4">
-                <th className="py-3 text-left text-xs font-medium uppercase">
-                  Name
-                </th>
-                <th className="hidden py-3 text-center text-xs font-medium uppercase md:table-cell">
-                  Rank
-                </th>
-                <th className="text-wrap py-3 text-center text-xs font-medium uppercase">
-                  <span className="hidden sm:block">Points</span>
-                  <span className="block sm:hidden">Pts</span>
-                </th>
-                {/* <th className="py-3 text-right text-xs font-medium uppercase">
+      <div className="overflow-x-auto">
+        <table className="min-w-full rounded-xl">
+          <thead className="pb-4">
+            <tr className="pb-4">
+              <th className="py-3 text-left text-xs font-medium uppercase">
+                Name
+              </th>
+              <th className="hidden py-3 text-center text-xs font-medium uppercase md:table-cell">
+                Rank
+              </th>
+              <th className="text-wrap py-3 text-center text-xs font-medium uppercase">
+                <span className="hidden sm:block">Points</span>
+                <span className="block sm:hidden">Pts</span>
+              </th>
+              {/* <th className="py-3 text-right text-xs font-medium uppercase">
                   Submissions
                 </th> */}
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData?.length > 0 ? (
-                // Create a new array to include the signed-in user at the top
-                [user, ...filteredData].map((entry: any, index: number) => {
-                  // Check if the current entry is the signed-in user
-                  if (user && entry.id === user.id) {
-                    // For the first instance (at the top)
-                    if (index === 0) {
-                      return;
-                      // return (
-                      //   <tr key={entry.id} className="border-b text-left">
-                      //     <td className="flex w-[200px] items-center justify-start space-x-2 py-4 md:w-[250px] lg:w-[350px]">
-                      //       <div className="table-cell pr-1 sm:hidden">1</div>
-                      //       <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
-                      //         <Image
-                      //           src={
-                      //             entry.image ||
-                      //             `https://avatar.vercel.sh/${entry.email}`
-                      //           }
-                      //           alt="Profile"
-                      //           fill={true}
-                      //           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      //           className="overflow-hidden rounded-full object-cover"
-                      //         />
-                      //       </div>
-                      //       <div className="flex w-0 flex-1 items-center text-sm font-bold">
-                      //         <span className="truncate">
-                      //           @
-                      //           {entry.username ||
-                      //             entry.email ||
-                      //             entry.name ||
-                      //             "User"}
-                      //         </span>
-                      //         <HoverBorderGradient
-                      //           containerClassName="ml-2 mr-auto w-min"
-                      //           className={`duration-400 w-min flex-1 truncate p-1 px-2 text-sm font-bold transition-all hover:bg-slate-900`}
-                      //           color={siteData.color1}
-                      //         >
-                      //           <span style={{ color: siteData.color1 }}>
-                      //             You
-                      //           </span>
-                      //         </HoverBorderGradient>
-                      //       </div>
-                      //     </td>
-                      //     <td className="hidden py-4 text-center md:table-cell">
-                      //       {filteredData.findIndex(
-                      //         (usr) => usr.id === user.id,
-                      //       ) + 1}
-                      //     </td>
-                      //     <td className="py-4 text-center">
-                      //       {parseFloat(entry.points || "0").toFixed(2)}
-                      //     </td>
-                      //     {/* Display the points for the signed-in user */}
-                      //   </tr>
-                      // );
-                    } else {
-                      // For the second instance (where they appear in their actual position)
-                      return (
-                        <tr key={entry.id} className="border-b text-left">
-                          <td className="flex w-[200px] items-center justify-start space-x-2 py-4 md:w-[250px] lg:w-[350px]">
-                            <div className="table-cell pr-1 sm:hidden">1</div>
-                            <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
-                              <Image
-                                src={
-                                  entry.image ||
-                                  `https://avatar.vercel.sh/${entry.email}`
-                                }
-                                alt="Profile"
-                                fill={true}
-                                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                                className="overflow-hidden rounded-full object-cover"
-                              />
-                            </div>
-                            <div className="flex w-0 flex-1 items-center text-sm font-bold">
-                              <span className="truncate">
-                                @
-                                {entry.username ||
-                                  entry.email ||
-                                  entry.name ||
-                                  "User"}
-                              </span>
-                              <HoverBorderGradient
-                                containerClassName="ml-2 mr-auto w-min"
-                                className={`duration-400 w-min flex-1 truncate p-1 px-2 text-sm font-bold transition-all hover:bg-slate-900`}
-                                color={siteData.color1}
-                              >
-                                <span style={{ color: siteData.color1 }}>
-                                  You
-                                </span>
-                              </HoverBorderGradient>
-                            </div>
-                          </td>
-                          <td className="hidden py-4 text-center md:table-cell">
-                            {filteredData.findIndex(
-                              (usr) => usr.id === user.id,
-                            ) + 1}
-                          </td>
-                          <td className="py-4 text-center">
-                            {parseFloat(entry.points || "0").toFixed(2)}
-                          </td>
-                          {/* Display the points for the signed-in user */}
-                        </tr>
-                      );
-                    }
-                  } else if (index > 0) {
-                    // Render the row for other users
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData?.length > 0 ? (
+              // Create a new array to include the signed-in user at the top
+              [user, ...filteredData].map((entry: any, index: number) => {
+                // Check if the current entry is the signed-in user
+                if (user && entry.id === user.id) {
+                  // For the first instance (at the top)
+                  if (index === 0) {
+                    return;
+                    // return (
+                    //   <tr key={entry.id} className="border-b text-left">
+                    //     <td className="flex w-[200px] items-center justify-start space-x-2 py-4 md:w-[250px] lg:w-[350px]">
+                    //       <div className="table-cell pr-1 sm:hidden">1</div>
+                    //       <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
+                    //         <Image
+                    //           src={
+                    //             entry.image ||
+                    //             `https://avatar.vercel.sh/${entry.email}`
+                    //           }
+                    //           alt="Profile"
+                    //           fill={true}
+                    //           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    //           className="overflow-hidden rounded-full object-cover"
+                    //         />
+                    //       </div>
+                    //       <div className="flex w-0 flex-1 items-center text-sm font-bold">
+                    //         <span className="truncate">
+                    //           @
+                    //           {entry.username ||
+                    //             entry.email ||
+                    //             entry.name ||
+                    //             "User"}
+                    //         </span>
+                    //         <HoverBorderGradient
+                    //           containerClassName="ml-2 mr-auto w-min"
+                    //           className={`duration-400 w-min flex-1 truncate p-1 px-2 text-sm font-bold transition-all hover:bg-slate-900`}
+                    //           color={siteData.color1}
+                    //         >
+                    //           <span style={{ color: siteData.color1 }}>
+                    //             You
+                    //           </span>
+                    //         </HoverBorderGradient>
+                    //       </div>
+                    //     </td>
+                    //     <td className="hidden py-4 text-center md:table-cell">
+                    //       {filteredData.findIndex(
+                    //         (usr) => usr.id === user.id,
+                    //       ) + 1}
+                    //     </td>
+                    //     <td className="py-4 text-center">
+                    //       {parseFloat(entry.points || "0").toFixed(2)}
+                    //     </td>
+                    //     {/* Display the points for the signed-in user */}
+                    //   </tr>
+                    // );
+                  } else {
+                    // For the second instance (where they appear in their actual position)
                     return (
                       <tr key={entry.id} className="border-b text-left">
-                        <td className="flex w-[150px] items-center space-x-2 py-4 md:w-[200px] lg:w-[300px]">
-                          <div className="table-cell pr-1 sm:hidden">
-                            {index}
-                          </div>
+                        <td className="flex w-[200px] items-center justify-start space-x-2 py-4 md:w-[250px] lg:w-[350px]">
+                          <div className="table-cell pr-1 sm:hidden">1</div>
                           <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
                             <Image
                               src={
@@ -230,35 +177,83 @@ const MainLeaderboard = ({
                               className="overflow-hidden rounded-full object-cover"
                             />
                           </div>
-                          <span className="ml-2 w-0 flex-1 truncate text-sm font-bold">
-                            @
-                            {entry.username ||
-                              entry.email ||
-                              entry.name ||
-                              "User"}
-                          </span>
+                          <div className="flex w-0 flex-1 items-center text-sm font-bold">
+                            <span className="truncate">
+                              @
+                              {entry.username ||
+                                entry.email ||
+                                entry.name ||
+                                "User"}
+                            </span>
+                            <HoverBorderGradient
+                              containerClassName="ml-2 mr-auto w-min"
+                              className={`duration-400 w-min flex-1 truncate p-1 px-2 text-sm font-bold transition-all hover:bg-slate-900`}
+                              color={siteData.color1}
+                            >
+                              <span style={{ color: siteData.color1 }}>
+                                You
+                              </span>
+                            </HoverBorderGradient>
+                          </div>
                         </td>
                         <td className="hidden py-4 text-center md:table-cell">
-                          {index}
+                          {filteredData.findIndex((usr) => usr.id === user.id) +
+                            1}
                         </td>
                         <td className="py-4 text-center">
                           {parseFloat(entry.points || "0").toFixed(2)}
                         </td>
-                        {/* Display points for the other user */}
+                        {/* Display the points for the signed-in user */}
                       </tr>
                     );
                   }
-                })
-              ) : (
-                <tr>
-                  <td className="pt-4 text-center">No data available</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
+                } else if (index > 0) {
+                  // Render the row for other users
+                  return (
+                    <tr key={entry.id} className="border-b text-left">
+                      <td className="flex w-[150px] items-center space-x-2 py-4 md:w-[200px] lg:w-[300px]">
+                        <div className="table-cell pr-1 sm:hidden">{index}</div>
+                        <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
+                          <Image
+                            src={
+                              entry.image ||
+                              `https://avatar.vercel.sh/${entry.email}`
+                            }
+                            alt="Profile"
+                            fill={true}
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            className="overflow-hidden rounded-full object-cover"
+                          />
+                        </div>
+                        <span className="ml-2 w-0 flex-1 truncate text-sm font-bold">
+                          @
+                          {entry.username ||
+                            entry.email ||
+                            entry.name ||
+                            "User"}
+                        </span>
+                      </td>
+                      <td className="hidden py-4 text-center md:table-cell">
+                        {index}
+                      </td>
+                      <td className="py-4 text-center">
+                        {parseFloat(entry.points || "0").toFixed(2)}
+                      </td>
+                      {/* Display points for the other user */}
+                    </tr>
+                  );
+                }
+              })
+            ) : loading ? (
+              <LoadingDots color="gray" />
+            ) : null}
+          </tbody>
+        </table>
+        {filteredData.length === 0 && !loading && (
+          <div className="pt-4 text-center">No data available</div>
+        )}
       </div>
-    )
+    </div>
   );
 };
 
