d67da7e8e409b923cad6e4a8e4e40bbc860061d9
BrunoCecco <bruno.ceccolini@gmail.com>
2024-11-20 03:00:47 +0000

    cards on how to play

--- app/[domain]/howtoplay/page.tsx
+++ app/[domain]/howtoplay/page.tsx
@@ -1,4 +1,5 @@
 import UserSettings from "@/components/settings/userSettings";
+import { CardSpotlight } from "@/components/ui/cardSpotlight";
 
 export default async function SettingsPage() {
   // explain how to play with beautiful layouy
@@ -6,44 +7,44 @@ export default async function SettingsPage() {
     <div>
       <h1 className="mb-6 text-3xl font-bold text-white">How to Play</h1>
 
-      <div className="mb-8 rounded-lg bg-slate-700 p-6 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-white">
+      <CardSpotlight className="mb-8 rounded-lg p-6 shadow-lg">
+        <h2 className=" relative z-20 mb-4 text-2xl font-semibold text-white">
           1. Enter a Competition
         </h2>
-        <p className="mb-2 text-gray-300">
+        <p className="relative z-20 mb-2 text-gray-300">
           Browse our active competitions and click &ldquo;Play&ldquo;.
         </p>
-      </div>
+      </CardSpotlight>
 
-      <div className="mb-8 rounded-lg bg-slate-700 p-6 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-white">
+      <CardSpotlight className="mb-8 rounded-lg p-6 shadow-lg">
+        <h2 className="relative z-20 mb-4 text-2xl font-semibold text-white">
           2. Answer Questions
         </h2>
-        <p className="mb-2 text-gray-300">Submit your answers!</p>
-      </div>
+        <p className="relative z-20 mb-2 text-gray-300">Submit your answers!</p>
+      </CardSpotlight>
 
-      <div className="mb-8 rounded-lg bg-slate-700 p-6 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-white">
+      <CardSpotlight className="mb-8 rounded-lg p-6 shadow-lg">
+        <h2 className="relative z-20 mb-4 text-2xl font-semibold text-white">
           3. Earn Points
         </h2>
-        <p className="mb-2 text-gray-300">
+        <p className="relative z-20  mb-2 text-gray-300">
           Points will be calculated after the event and you will be placed on a
           leaderboard.
         </p>
-      </div>
+      </CardSpotlight>
 
-      <div className="rounded-lg bg-slate-700 p-6 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-white">
+      <CardSpotlight className="mb-8 rounded-lg p-6 shadow-lg">
+        <h2 className="relative z-20  mb-4 text-2xl font-semibold text-white">
           4. Win Rewards
         </h2>
-        <p className="mb-2 text-gray-300">
+        <p className="relative z-20  mb-2 text-gray-300">
           Top performers in each competition have a chance to win exciting
           rewards.
         </p>
         <p className="text-gray-300">
           Check the competition details for specific reward information.
         </p>
-      </div>
+      </CardSpotlight>
     </div>
   );
 }
