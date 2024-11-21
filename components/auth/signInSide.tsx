a4d6f9c60b4b361fa3d9d933bdd1c985fb1e59f0
BrunoCecco <bruno.ceccolini@gmail.com>
2024-11-21 01:59:00 +0000

    login fix

--- components/auth/signInSide.tsx
+++ components/auth/signInSide.tsx
@@ -56,7 +56,11 @@ function ColorSchemeToggle(props: IconButtonProps) {
         height: 20,
       }}
     >
-      {mode === "light" ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+      {mode === "light" ? (
+        <DarkModeRoundedIcon className="text-gray-700" />
+      ) : (
+        <LightModeRoundedIcon />
+      )}
     </IconButton>
   );
 }
@@ -140,7 +144,7 @@ export default function UserSignUp({
   };
 
   return (
-    <CssVarsProvider disableTransitionOnChange>
+    <CssVarsProvider>
       <CssBaseline />
       <GlobalStyles
         styles={{
@@ -150,258 +154,214 @@ export default function UserSignUp({
           },
         }}
       />
-      <Box
-        sx={(theme) => ({
-          width: { xs: "100%", md: "50vw" },
-          transition: "width var(--Transition-duration)",
-          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
-          position: "relative",
-          zIndex: 1,
-          display: "flex",
-          justifyContent: "flex-end",
-          backdropFilter: "blur(12px)",
-          backgroundColor: "rgba(255 255 255 / 0.2)",
-          [theme.getColorSchemeSelector("dark")]: {
-            backgroundColor: "rgba(19 19 24 / 0.4)",
-          },
-        })}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            minHeight: "100dvh",
-            width: "100%",
-            px: 2,
-          }}
+      <div className="bg-white dark:bg-[rgba(19,19,24,0.4)]">
+        <div
+          className="duration-[var(--Transition-duration)] delay-[calc(var(--Transition-duration)+0.1s)] relative z-10 flex w-full justify-end bg-[rgba(255,255,255,0.2)] transition-all dark:bg-[rgba(19,19,24,0.4)] md:w-[50vw]"
+          style={{ backdropFilter: "blur(12px)" }}
         >
-          <Box
-            component="header"
-            sx={{ py: 2, display: "flex", justifyContent: "end" }}
-          >
-            <ColorSchemeToggle />
-          </Box>
-          <Box
-            component="main"
-            sx={{
-              my: "auto",
-              pb: 2,
-              display: "flex",
-              flexDirection: "column",
-              gap: 1,
-              width: 400,
-              maxWidth: "100%",
-              mx: "auto",
-              borderRadius: "sm",
-              "& form": {
+          <div className="flex min-h-[100vh] w-full flex-col px-2">
+            <header className="flex justify-end pt-2">
+              <ColorSchemeToggle />
+            </header>
+            <div
+              className="m-auto pb-2"
+              style={{
                 display: "flex",
                 flexDirection: "column",
-                gap: 2,
-              },
-              [`& .MuiFormLabel-asterisk`]: {
-                visibility: "hidden",
-              },
-            }}
-          >
-            <div className="flex justify-center pb-4">
-              <Image
-                src={siteData?.logo ?? "/logo.png"}
-                width={100}
-                height={100}
-                alt="Site Logo"
-                className="flex h-32 w-32 object-contain"
-              />
-            </div>
-            <Stack sx={{ gap: 3, mb: emailExists ? 2 : 0 }}>
-              <Stack sx={{ gap: 1 }}>
-                {emailExists ? (
-                  <Typography component="h1" level="h3">
-                    <div className="font-space">Sign In & Play </div>
-                  </Typography>
-                ) : (
-                  <Typography component="h1" level="h3">
-                    <div className="font-space">
-                      Bravo&nbsp;🎉 &nbsp;Let the Competition begin!
-                    </div>
-                  </Typography>
-                )}
-              </Stack>
-              {emailExists && (
-                <>
-                  <Button
-                    onClick={() => handleGoogleSignin()}
-                    className="dark:bg-gray-800 dark:hover:bg-gray-700"
-                    variant="soft"
-                    color="neutral"
-                    fullWidth
-                    startDecorator={
-                      <GoogleIcon className="mr-2 text-2xl text-black dark:text-gray-200" />
-                    }
-                  >
-                    <div className="font-space">Continue with Google</div>
-                  </Button>
-                  <Button
-                    onClick={() => handleAppleSignin()}
-                    className="dark:bg-gray-800 dark:hover:bg-gray-700"
-                    variant="soft"
-                    color="neutral"
-                    fullWidth
-                    startDecorator={
-                      <AppleIcon className="mr-2 text-2xl text-black dark:text-gray-200" />
-                    }
-                  >
-                    <div className="font-space">Continue with Apple</div>
-                  </Button>
-                </>
-              )}
-            </Stack>
-
-            {emailExists ? (
-              <Divider
-                sx={(theme) => ({
-                  [theme.getColorSchemeSelector("light")]: {
-                    color: { xs: "#FFF", md: "text.tertiary" },
-                  },
-                })}
+                gap: 1,
+                width: 400,
+                maxWidth: "100%",
+                borderRadius: "sm",
+              }}
+            >
+              <div className="flex justify-center pb-4">
+                <Image
+                  src={siteData?.logo ?? "/logo.png"}
+                  width={100}
+                  height={100}
+                  alt="Site Logo"
+                  className="flex h-32 w-32 object-contain"
+                />
+              </div>
+              <div
+                className={`flex flex-col gap-3 ${emailExists ? "mb-2" : "mb-0"}`}
               >
-                <div className="font-space">or</div>
-              </Divider>
-            ) : (
-              <Typography>
-                <div className="font-space">
-                  Choose a username. This is what people will see on the
-                  leaderboard when you enter competitions.
+                <div className="gap-1">
+                  {emailExists ? (
+                    <Typography component="h1" level="h3">
+                      <div className="font-space">Sign In & Play </div>
+                    </Typography>
+                  ) : (
+                    <Typography component="h1" level="h3">
+                      <div className="font-space">
+                        Bravo&nbsp;🎉 &nbsp;Let the Competition begin!
+                      </div>
+                    </Typography>
+                  )}
                 </div>
-              </Typography>
-            )}
-
-            <Stack sx={{ gap: 4, mt: emailExists ? 2 : 0 }}>
-              <form>
-                {emailExists ? (
-                  <FormControl required>
-                    <FormLabel>
-                      <div className="font-space">Email</div>
-                    </FormLabel>
-                    <input
-                      id="email"
-                      type="email"
-                      autoComplete="email"
-                      onChange={(e) => setEmail(e.target.value)}
-                      className="mt-1 block w-full rounded-md border border-gray-300 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
-                    />
-                  </FormControl>
-                ) : (
+                {emailExists && (
                   <>
-                    <FormControl required>
-                      <FormLabel className="font-space">
-                        <div className="font-space">Username</div>
-                      </FormLabel>
-                      <input
-                        id="username"
-                        type="username"
-                        autoComplete="username"
-                        onChange={(e) => setUsername(e.target.value)}
-                        className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
-                      />
-                    </FormControl>
-                    <FormControl>
-                      <FormLabel>
-                        <div className="font-space">Full Name</div>
-                      </FormLabel>
-                      <input
-                        id="fullname"
-                        type="fullname"
-                        autoComplete="fullname"
-                        onChange={(e) => setName(e.target.value)}
-                        className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
-                      />
-                    </FormControl>
+                    <Button
+                      onClick={() => handleGoogleSignin()}
+                      className="dark:bg-gray-800 dark:hover:bg-gray-700"
+                      variant="soft"
+                      color="neutral"
+                      fullWidth
+                      startDecorator={
+                        <GoogleIcon className="mr-2 text-2xl text-black dark:text-gray-200" />
+                      }
+                    >
+                      <div className="font-space">Continue with Google</div>
+                    </Button>
+                    <Button
+                      onClick={() => handleAppleSignin()}
+                      className="dark:bg-gray-800 dark:hover:bg-gray-700"
+                      variant="soft"
+                      color="neutral"
+                      fullWidth
+                      startDecorator={
+                        <AppleIcon className="mr-2 text-2xl text-black dark:text-gray-200" />
+                      }
+                    >
+                      <div className="font-space">Continue with Apple</div>
+                    </Button>
                   </>
                 )}
-                <Stack sx={{ gap: 4, mt: 2 }}>
-                  <LoginButton
-                    email={email}
-                    username={username}
-                    localAnswers={localAnswers}
-                    competitionSlug={competitionSlug}
-                    name={name}
-                    setEmailExists={setEmailExists}
-                  />
-                  {emailExists && (
-                    <p className="text-xs">
-                      By continuing you agree to the{" "}
-                      <a
-                        className="font-semibold text-gray-800 dark:text-gray-300"
-                        href="https://vycto.com/terms&conditions"
-                        rel="noreferrer"
-                        target="_blank"
-                      >
-                        Terms of use
-                      </a>{" "}
-                      and{" "}
-                      <a
-                        href="https://vycto.com/privacy-policy"
-                        rel="noreferrer"
-                        target="_blank"
-                        className="font-semibold text-gray-800 dark:text-gray-300"
-                      >
-                        Privacy Policy
-                      </a>
-                    </p>
-                  )}
-                </Stack>
-              </form>
-            </Stack>
-          </Box>
-          <Box component="footer" sx={{ py: 3 }}>
-            <Typography level="body-xs" sx={{ textAlign: "center" }}>
-              <div className="flex items-center justify-center">
-                <div className="pr-1.5 font-space">powered by</div>
-                <div className="relative h-8 w-12">
-                  {/* White logo for dark mode */}
-                  <Image
-                    src="/vyctoLogoWhite.png"
-                    alt="Vycto Logo"
-                    layout="fill"
-                    objectFit="contain"
-                    className="hidden h-full w-full dark:block"
-                  />
-                  {/* Blue logo for light mode */}
-                  <Image
-                    src="/vyctoLogoBlue.png"
-                    alt="Vycto Logo"
-                    layout="fill"
-                    objectFit="contain"
-                    className="h-full w-full dark:hidden"
-                  />
-                </div>
               </div>
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        sx={(theme) => ({
-          height: "100%",
-          position: "fixed",
-          right: 0,
-          top: 0,
-          bottom: 0,
-          left: { xs: 0, md: "50vw" },
-          transition:
-            "background-image var(--Transition-duration), left var(--Transition-duration) !important",
-          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
-          backgroundColor: "background.level1",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundImage: siteData?.loginBanner ?? "url(/loginBanner.png)",
-          [theme.getColorSchemeSelector("dark")]: {
-            backgroundImage:
-              siteData?.loginBannerDark ?? "url(/loginBannerDark.png)",
-          },
-        })}
-      />
+
+              {emailExists ? (
+                <Divider
+                  sx={(theme) => ({
+                    [theme.getColorSchemeSelector("light")]: {
+                      color: { xs: "#FFF", md: "text.tertiary" },
+                    },
+                  })}
+                >
+                  <div className="font-space">or</div>
+                </Divider>
+              ) : (
+                <Typography>
+                  <div className="font-space">
+                    Choose a username. This is what people will see on the
+                    leaderboard when you enter competitions.
+                  </div>
+                </Typography>
+              )}
+
+              <div
+                className={`flex flex-col gap-4 ${emailExists ? "mt-2" : "mt-0"}`}
+              >
+                <form className="flex flex-col gap-2">
+                  {emailExists ? (
+                    <FormControl required>
+                      <FormLabel>
+                        <div className="font-space">Email</div>
+                      </FormLabel>
+                      <input
+                        id="email"
+                        type="email"
+                        autoComplete="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="mt-1 block w-full rounded-md border border-gray-300 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
+                      />
+                    </FormControl>
+                  ) : (
+                    <>
+                      <FormControl required>
+                        <FormLabel className="font-space">
+                          <div className="font-space">Username</div>
+                        </FormLabel>
+                        <input
+                          id="username"
+                          type="username"
+                          autoComplete="username"
+                          onChange={(e) => setUsername(e.target.value)}
+                          className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
+                        />
+                      </FormControl>
+                      <FormControl>
+                        <FormLabel>
+                          <div className="font-space">Full Name</div>
+                        </FormLabel>
+                        <input
+                          id="fullname"
+                          type="fullname"
+                          autoComplete="fullname"
+                          onChange={(e) => setName(e.target.value)}
+                          className="mt-1 block w-full rounded-md border border-gray-300 px-2 py-1.5 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-700 dark:bg-gray-800"
+                        />
+                      </FormControl>
+                    </>
+                  )}
+                  <div className="mt-2 flex flex-col gap-4">
+                    <LoginButton
+                      email={email}
+                      username={username}
+                      localAnswers={localAnswers}
+                      competitionSlug={competitionSlug}
+                      name={name}
+                      setEmailExists={setEmailExists}
+                    />
+                    {emailExists && (
+                      <p className="text-xs">
+                        By continuing you agree to the{" "}
+                        <a
+                          className="font-semibold text-gray-800 dark:text-gray-300"
+                          href="https://vycto.com/terms&conditions"
+                          rel="noreferrer"
+                          target="_blank"
+                        >
+                          Terms of use
+                        </a>{" "}
+                        and{" "}
+                        <a
+                          href="https://vycto.com/privacy-policy"
+                          rel="noreferrer"
+                          target="_blank"
+                          className="font-semibold text-gray-800 dark:text-gray-300"
+                        >
+                          Privacy Policy
+                        </a>
+                      </p>
+                    )}
+                  </div>
+                </form>
+              </div>
+            </div>
+            <footer className="py-3">
+              <Typography level="body-xs" sx={{ textAlign: "center" }}>
+                <div className="flex items-center justify-center">
+                  <div className="pr-1.5 font-space">powered by</div>
+                  <div className="relative h-8 w-12">
+                    {/* White logo for dark mode */}
+                    <Image
+                      src="/vyctoLogoWhite.png"
+                      alt="Vycto Logo"
+                      layout="fill"
+                      objectFit="contain"
+                      className="hidden h-full w-full dark:block"
+                    />
+                    {/* Blue logo for light mode */}
+                    <Image
+                      src="/vyctoLogoBlue.png"
+                      alt="Vycto Logo"
+                      layout="fill"
+                      objectFit="contain"
+                      className="h-full w-full dark:hidden"
+                    />
+                  </div>
+                </div>
+              </Typography>
+            </footer>
+          </div>
+        </div>
+        <div
+          className={`fixed bottom-0 right-0 top-0 h-full bg-[url(/loginBanner.png)] dark:bg-[url(/loginBannerDark.png)] ${"left-0 md:left-[50vw]"} duration-[var(--Transition-duration)] delay-[calc(var(--Transition-duration)+0.1s)] bg-cover bg-center bg-no-repeat`}
+          style={{
+            transition: "background-image 0.4s, left 0.4s",
+          }}
+        />
+      </div>
     </CssVarsProvider>
   );
 }
