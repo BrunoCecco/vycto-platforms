a66ec2d343263e76c2669928443e5012b7e83642
BrunoCecco <bruno.ceccolini@gmail.com>
2024-12-17 19:24:25 -0500

    update slider

--- components/competitions/slider.tsx
+++ components/competitions/slider.tsx
@@ -61,10 +61,11 @@ const Slider: FC<{
               max="90"
               startContent={<div className="">{MIN}</div>}
               endContent={<div className="relative">{MAX}</div>}
+              defaultValue={initialValue}
               value={initialValue}
               disabled={disabled}
               onChange={handleChange}
-              onBlur={handleBlur}
+              onDragLeave={handleBlur}
               className="h-3 w-full appearance-none"
             />
           </div>
