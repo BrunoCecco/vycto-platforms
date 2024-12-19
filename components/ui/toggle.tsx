29da43407de20c521caed3a4765fa6e6753cbaa8
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-12-19 11:33:01 -0500

    Update toggle.tsx

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 
 const Toggle = ({
+  id,
+  key,
+  isSelected,
   onToggle,
+  label
 }: {
-  onToggle?: (toggled: boolean) => void
+  id: string,
+  key: string,
+  isSelected: boolean,  
+  onToggle?: (toggled: boolean) => void,
+  label?: string
 }) => {
-  const [toggled, setToggled] = useState(true)
+  const [toggled, setToggled] = useState(isSelected)
 
   const handleToggle = () => {
     const newState = !toggled
@@ -16,20 +24,25 @@ const Toggle = ({
   }
 
   return (
-    <button
-      className={
-        'relative h-[25px] w-[45px] translate-x-1 translate-y-1 cursor-pointer  rounded-full bg-[#222222]'
-      }
-      onClick={handleToggle}
-    >
-      <span
-        className={`flex h-full w-full items-center justify-center rounded-full border-[2px]  border-[#222222] ${toggled ? 'translate-x-[3px] translate-y-[-3px] bg-[#ff527a]' : ' bg-gray-300'} p-[2px] transition-all duration-300`}
+    <div className="flex items-center gap-2">
+      <button
+        id={id}
+        key={key}      
+        className={
+          'relative h-[25px] w-[45px] translate-x-1 translate-y-1 cursor-pointer  rounded-full bg-[#222222]'
+        }
+        onClick={handleToggle}
       >
         <span
-          className={`aspect-square h-full transform rounded-full border-[2px] border-[#222222] bg-white shadow-lg ${toggled ? 'translate-x-[10px]' : 'translate-x-[-10px]'} transition-all duration-300`}
-        />
-      </span>
-    </button>
+          className={`flex h-full w-full items-center justify-center rounded-full border-[2px]  border-[#222222] ${toggled ? 'translate-x-[3px] translate-y-[-3px] bg-[#ff527a]' : ' bg-gray-300'} p-[2px] transition-all duration-300`}
+        >
+          <span
+            className={`aspect-square h-full transform rounded-full border-[2px] border-[#222222] bg-white shadow-lg ${toggled ? 'translate-x-[10px]' : 'translate-x-[-10px]'} transition-all duration-300`}
+          />
+        </span>
+      </button>
+      <div>{label}</div>
+    </div>
   )
 }
 
