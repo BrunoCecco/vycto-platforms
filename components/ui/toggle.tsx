b8df691369a66c841a4a77b8208598736feb12e5
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2024-12-19 12:11:21 -0500

    Update toggle.tsx

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -29,15 +29,15 @@ const Toggle = ({
         id={id}
         key={key}      
         className={
-          'relative cursor-pointer h-[25px] w-[45px] translate-x-1 translate-y-1 cursor-pointer  rounded-full bg-[#222222]'
+          'relative cursor-pointer h-[25px] w-[45px] translate-x-1 translate-y-1 cursor-pointer rounded-full bg-foreground'
         }
         onClick={handleToggle}
       >
         <span
-          className={`flex h-full w-full items-center justify-center rounded-full border-[2px]  border-[#222222] ${toggled ? 'translate-x-[3px] translate-y-[-3px] bg-[#ff527a]' : ' bg-gray-300'} p-[2px] transition-all duration-300`}
+          className={`flex h-full w-full items-center justify-center rounded-sm border-[2px]  ${toggled ? 'translate-x-[3px] translate-y-[-3px] bg-primary' : ' bg-content3'} p-[2px] transition-all duration-300`}
         >
           <span
-            className={`aspect-square h-full transform rounded-full border-[2px] border-[#222222] bg-white shadow-lg ${toggled ? 'translate-x-[10px]' : 'translate-x-[-10px]'} transition-all duration-300`}
+            className={`aspect-square h-full transform border-[2px] rounded-sm bg-background shadow-lg ${toggled ? 'translate-x-[10px]' : 'translate-x-[-10px]'} transition-all duration-300`}
           />
         </span>
       </div>
