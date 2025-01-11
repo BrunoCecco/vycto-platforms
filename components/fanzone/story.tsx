024c8434f95f224554a1b07b9c5ac8f10283df2e
Bruno Ceccolini <bruno.ceccolini@gmail.com>
2025-01-11 20:18:45 +0000

    Update story.tsx

--- components/fanzone/story.tsx
+++ components/fanzone/story.tsx
@@ -11,7 +11,7 @@ export function Story({ siteData }: { siteData: SelectSite }) {
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-auto rounded-md antialiased">
       <div className="mx-auto p-4">
         <h1 className="relative z-10 bg-gradient-to-b from-default-400 to-default-600  bg-clip-text p-2 text-center font-sans text-2xl  font-bold text-transparent md:text-6xl">
-          {t("joinfamily").replace("<site>",siteData.name)}
+          {t("joinfamily").replace("<site>",siteData.name||"")}
         </h1>
         <TextGenerateEffect words={siteData.story || ""} />
       </div>
