8ae137370ff2ebc72b0bae59822bff281bafcd28
BrunoCecco <bruno.ceccolini@gmail.com>
2025-01-26 20:49:19 +0000

    update

--- components/settings/cookieBanner.tsx
+++ components/settings/cookieBanner.tsx
@@ -40,26 +40,29 @@ export default function CookieBanner() {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex flex-col gap-2 rounded-lg bg-background p-4">
-      <p>
-        We use mandatory cookies for authentication and handling your sessions.
-      </p>
-      {consentGiven === "undecided" && (
-        <div className="flex flex-col gap-2 shadow-lg">
-          <p>
-            We also use optional analytics cookies to improve your experience
-            and help us understand how we can improve our product.
-          </p>
-          <div className="flex items-center gap-2">
-            <Button onClick={handleAcceptOptionalCookies}>
-              Accept Optional Cookies
-            </Button>
-            <Button onClick={handleDeclineOptionalCookies}>
-              Decline Cookies
-            </Button>
+    cookieConsentGiven() === "undecided" && (
+      <div className="fixed bottom-0 left-0 right-0 flex flex-col gap-2 rounded-lg bg-background p-4">
+        <p>
+          We use mandatory cookies for authentication and handling your
+          sessions.
+        </p>
+        {consentGiven === "undecided" && (
+          <div className="flex flex-col gap-2 shadow-lg">
+            <p>
+              We also use optional analytics cookies to improve your experience
+              and help us understand how we can improve our product.
+            </p>
+            <div className="flex items-center gap-2">
+              <Button onClick={handleAcceptOptionalCookies}>
+                Accept Optional Cookies
+              </Button>
+              <Button onClick={handleDeclineOptionalCookies}>
+                Decline Cookies
+              </Button>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    )
   );
 }
